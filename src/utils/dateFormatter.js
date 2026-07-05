df24288ad8332d97031a1445f7ec6f3b9bf07d75
Manikandan Dhanapal <manikandanthedeveloper@users.noreply.github.com>
2026-07-06 01:25:59 +0530

    removed unnecessary field

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -1,8 +1,3 @@
-const dateFormatterIntl = new Intl.DateTimeFormat("en-US", {
-	day: "2-digit",
-	month: "2-digit",
-	year: "numeric",
-});
 const monthLongFormatter = new Intl.DateTimeFormat("en-US", {
 	month: "long",
 });
